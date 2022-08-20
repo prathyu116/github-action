cdf45e235a542e2b685a6f2c83c0cd7c8a11efd3
prathyu Prasad <prathyuprasad04669@gmail.com>
2022-08-20 23:00:27 +0530

    sd1

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const multiplyBy2 = (x) => x * 2;
+const multiplyBy3 = (x) => x * 3;
+const multiplyBy4 = (x) => x * 4;
+
+const capitaliseName = (name) => name[0].toUpperCase() + name.slice(1);
+
+module.exports = {
+  multiplyBy2,
+  multiplyBy3,
+  multiplyBy4,
+  capitaliseName,
+};
